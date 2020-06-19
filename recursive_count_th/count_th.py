29119dad68825752f69b5fad9d4db64196ace981
Julia Vu <juliadvu@gmail.com>
2020-06-19 11:01:34 -0500

    working on the UPER for count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,6 +3,31 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
+# U
+#takes in word as parameter, must return a count (need a counter that will increment at +1)
+#look for words that contain the "th" string
+#going to assume word is similar to an array that has indices to be able to search for "th" within it
+
+#use recursion to call the function within itself
+
+
+
+# P
+
+#for count, see if it could be passed in as a parameter and set to default of 0. To initialize the counter
+#conditions to consider
+#word needs at least the two letters "th"
+#if the word contains less than or equal to 1 letter then just return back the empty count of 0
+
+#if the word contains at least two letters, with th in its index then 
+#return the count back incremented by count +1
+
+# E
+# R#
+
+
 def count_th(word):
     
     # TBC
