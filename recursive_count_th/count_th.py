cd77aa3cbe68c54c28acc9d0ea2d714a095872bd
Julia Vu <juliadvu@gmail.com>
2020-06-19 11:18:10 -0500

    tests for recursive count seems to be passing

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -17,6 +17,7 @@ Your function must utilize recursion. It cannot contain any loops.
 # P
 
 #for count, see if it could be passed in as a parameter and set to default of 0. To initialize the counter
+#def count_th(word, count =0):
 #conditions to consider
 #word needs at least the two letters "th"
 #if the word contains less than or equal to 1 letter then just return back the empty count of 0
@@ -24,12 +25,31 @@ Your function must utilize recursion. It cannot contain any loops.
 #if the word contains at least two letters, with th in its index then 
 #return the count back incremented by count +1
 
+#lastly, we need to call our recursion in our last return statement that will return our parameters of word and count
+
 # E
+#how to make sure word array contains the string "th" in it?
+# "th" has to exist in the index of the word array, use slicing method?
+
 # R#
 
 
+# def count_th(word, count = 0):
+#     #if length of word has 1 or 0 letters then return count 0 value
+#     if len(word) <= 1:
+#         return count
+#     #if the first two letters is found in the word array then add count +1
+#     if word[:2] =="th":
+#         count +=1
+#     #return function recursively to find the first word that contains 'th', and the count as well
+#     return count_th(word[1:], count)
+
+
+#2nd method
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    if len(word) < 2:
+      return 0
+    elif word[0:2] == 'th':
+        return 1 + count_th(word[1:])
+    else:
+        return count_th(word[1:])
